e6f1292a4cbde0dfc3057baa648e138d284af2b9
Anatoliy <a.bygyb@gmail.com>
2017-04-17 20:46:05 +0300

    add signin

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 var index = require('./routes/index');
-var users = require('./routes/users');
+var routeUser = require('./routes/user');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
 mongoose.connect('localhost:27017/shopping');
@@ -28,15 +29,16 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({
   secret: 'mysecret',
   resave: false,
   saveUninitialized: false,
   store: new MongoStore({
-      url: 'mongodb://localhost/test-app',
-      autoRemove: 'disabled'
-  })
+      mongooseConnection: mongoose.connection
+  }),
+  cookie: {maxAge: 180 * 60 * 1000}
 }));
 
 app.use(flash());
@@ -44,8 +46,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session.id;
+  next();
+});
+app.use('/user', routeUser);
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var csrf = require('csurf');
-var csrfProtection = csrf();
-var passport = require('passport');
 var Product = require('../models/product');
 
-var session = require('express-session');
-
-router.use(csrfProtection);
 
 router.get('/', function(req, res, next) {
   Product.find(function (err, docs) {
@@ -16,25 +10,6 @@ router.get('/', function(req, res, next) {
           products: docs
       });
   });
-});
-
-router.get('/user/signup', function (req, res, next) {
-   var messages = req.flash('error');
-   res.render('user/signup', {
-       csrfToken: req.csrfToken(),
-       messages: messages,
-       hasError: messages.length > 0
-   });
-});
-
-router.post('/user/signup', passport.authenticate('local.signup', {
-    successRedirect: '/user/profile',
-    failureRedirect: '/user/signup',
-    failureFlash: true
-}));
-
-router.get('/user/profile', function (req, res, next) {
-    res.render('user/profile');
 });
 
 module.exports = router;
